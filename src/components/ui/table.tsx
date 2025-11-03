251f53a197268afd6945a9c7e35676ce3bcb6a93
Vishal Arjoon <vishalarjoon029@gmail.com>
2025-11-03 12:50:45 +0000

    - Can you make the header background color as dark blue and make the tex

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -20,7 +20,7 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b bg-blue-100 dark:bg-blue-900/30", className)} {...props} />
+  <thead ref={ref} className={cn("[&_tr]:border-b bg-blue-800 dark:bg-blue-900", className)} {...props} />
 ))
 TableHeader.displayName = "TableHeader"
 
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-foreground [&:has([role=checkbox])]:pr-0",
+      "h-12 px-4 text-left align-middle font-medium text-primary-foreground [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
