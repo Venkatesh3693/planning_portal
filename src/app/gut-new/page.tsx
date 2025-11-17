f54fb4ab9eb6ab200e020c96e6ea602d53ef5ebc
Vishal Arjoon <vishalarjoon029@gmail.com>
2025-11-17 06:38:29 +0000

    Try fixing this error: `Runtime Error: Error: Can't find variable: onDro

--- src/app/gut-new/page.tsx
+++ src/app/gut-new/page.tsx
@@ -940,7 +940,7 @@ function GanttPageContent() {
                       dates={dates}
                       scheduledProcesses={processesForGantt}
                       allProcesses={scheduledProcesses}
-                      onDrop={onDrop}
+                      onDrop={handleDropOnChart}
                       onUndoSchedule={handleUndoSchedule}
                       onProcessDragStart={handleDragStart}
                       onSplitProcess={handleOpenSplitDialog}
