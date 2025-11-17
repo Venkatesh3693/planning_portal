ea3efb3cb437826328dd8430ef62d24606930346
Vishal Arjoon <vishalarjoon029@gmail.com>
2025-11-17 08:04:44 +0000

    Try fixing this error: `Runtime Error: Error: Can't find variable: start

--- src/app/gut-new/page.tsx
+++ src/app/gut-new/page.tsx
@@ -3,7 +3,7 @@
 'use client';
 
 import { useState, useMemo, useEffect } from 'react';
-import { addDays, startOfToday, getDay, set, isAfter, isBefore, addMinutes, compareAsc, compareDesc, subDays, startOfWeek, format } from 'date-fns';
+import { addDays, startOfToday, getDay, set, isAfter, isBefore, addMinutes, compareAsc, compareDesc, subDays, startOfWeek, format, startOfDay } from 'date-fns';
 import { Header } from '@/components/layout/header';
 import GanttChart from '@/components/gantt-chart/gantt-chart';
 import { MACHINES, PROCESSES, WORK_DAY_MINUTES, SEWING_OPERATIONS_BY_STYLE } from '@/lib/data';
@@ -1094,5 +1094,6 @@ export default function GutNewPage() {
     <GanttPageContent />
   );
 }
+
 
 
